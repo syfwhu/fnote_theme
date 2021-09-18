4ad19a31794437bf3421cb8a446fa0ea0a5610b6
shiyanfei <shiyanfei@gdcert.com>
2021-09-18 08:32:24 +0800

    init: 初始化项目

---
+++ functions.php
@@ -0,0 +1,55 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+// 统计阅读数
+function get_post_view($archive){
+	$cid    = $archive->cid;
+	$db     = Typecho_Db::get();
+	$prefix = $db->getPrefix();
+	if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
+		$db->query('ALTER TABLE `' . $prefix . 'contents` ADD `views` INT(10) DEFAULT 0;');
+		echo 0;
+		return;
+	}
+	$row = $db->fetchRow($db->select('views')->from('table.contents')->where('cid = ?', $cid));
+	if ($archive->is('single')) {
+        $views = Typecho_Cookie::get('extend_contents_views');
+		if(empty($views)){
+			$views = array();
+		}else{
+			$views = explode(',', $views);
+		}
+        if(!in_array($cid,$views)){
+	        $db->query($db->update('table.contents')->rows(array('views' => (int) $row['views'] + 1))->where('cid = ?', $cid));
+            array_push($views, $cid);
+			$views = implode(',', $views);
+			Typecho_Cookie::set('extend_contents_views', $views); //记录查看cookie
+		}
+	}
+	echo $row['views'];
+}
+
+// 获取附件图片
+function thumb($obj) {
+	$attach = $obj->attachments(1)->attachment;
+	if(isset($attach->isImage) && $attach->isImage == 1){
+		$thumb = $attach->url;
+	}else{
+		$thumb = '/usr/themes/echo/img/00.png';
+	}
+	return $thumb;
+}
+
+// 留言加@
+function getPermalinkFromCoid($coid) {
+	$db = Typecho_Db::get();
+	$row = $db->fetchRow($db->select('author')->from('table.comments')->where('coid = ? AND status = ?', $coid, 'approved'));
+	if (empty($row)) return '';
+	return '<a href="#comment-'.$coid.'">@'.$row['author'].'</a>';
+}
+
+// 主题设置
+function themeConfig($form) {
+    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 可以在网站标题前加上一个 LOGO'));
+    $form->addInput($logoUrl);
+}
